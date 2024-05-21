a9cd0af2de5f767e9656e1617fbd54afd1539d5e
edwin_liu1 <edwin.liu25@ycdsbk12.ca>
2024-05-21 04:32:14 -0400

    gonna sleep

--- Objects.java
+++ Objects.java
@@ -2,73 +2,135 @@ import processing.core.PApplet;
 
 public class Objects{
 
+    static float x, y, velX, velY;
+
     static class Snowball{
-        float x, y, velX, velY;
-        double d;
+        double r;
         double g;
         boolean hidden = false;
         Snowball (float X, float Y, float XSpeed, float YSpeed, int size, double growth){
             x = X; y = Y;
             velX = XSpeed; velY = YSpeed;
-            d = size; g = growth;
+            r = size; g = growth;
             hidden = false;
-        }
-        public void update(){
-            x += velX;
-            y += velY;
-            d += g;
         }
         public void hide(){
             hidden = true;
         }
-        public int getX(){return Math.round(x);}
-        public int getY(){return Math.round(y);}
-        public int getR(){return Math.round( (int)d);}
+
+        public void update(){x += velX; y += velY; r += g;}
+        public void slowDown(){velX /= 2; velY /= 2; g /= 2;}
+        public void speedUp(){velX *= 2;velY *= 2; g *= 2;}
+
+        public float getX(){return x;}
+        public float getY(){return y;}
+        public float getR(){return (float)r;}
     }
 
     static class Wall{
-        float x, y, velX, velY;
         int w, h;
         Wall(float X, float Y, int width, int height, float XSpeed, float YSpeed){
             x = X; y = Y;
             velX = XSpeed; velY = YSpeed;
             w = width; h = height;
         }
-        public void update(){
-            x += velX;
-            y += velY;
-        }
+
+        public void update(){x += velX; y += velY;}
+        public void slowDown(){velX /= 2; velY /= 2;}
+        public void speedUp(){velX *= 2;velY *= 2;}
+
+        public float getX(){return x;}
+        public float getY(){return y;}
+        public float getW(){return w;}
+        public float getH(){return h;}
     }
 
+    // probably the most complicated object and it's just for aescthetics
     static class Wind extends PApplet{
-        float x, y, velX, velY;
-        double wind;
+        static double wind;
         int w, h;
+        WindLine[] lines;
         Wind(float X, float Y, int width, int height, float XSpeed, float YSpeed, double windSpeed){
-            windLine[] lines = new windLine[ Math.round( (float)Math.sqrt( Math.min(width, 2) * Math.pow(height,2) ) )];
-            for (windLine line: lines){
+            x = X; y = Y;
+            velX = XSpeed; velY = YSpeed;
+            w = width; h = height;
+            wind = windSpeed;
+            lines = new WindLine[Math.round( (float)Math.sqrt(w * h) )];
+            for (int i = 0; i < lines.length; i++){
                 float lX = random(x-w, x+w);
                 float lY = random(y-h, y+h);
-                line = new windLine(lX, lY, lX + ((float)Math.pow(wind, 2) / w), lY, wind);
+                lines[i] = new WindLine(lX, lY);
             }
         }
 
-        static class windLine{
-            float x, y, x2, y2;
-            double s;
-            windLine(float X, float Y, float X2, float Y2, double windSpeed){
-                x = X; y = Y;
-                x2 = X2; y2 = Y2;
-                s = windSpeed;
+        public void update(){
+            x += velX; y += velY;
+            for (WindLine line : lines){
+                line.update();
             }
+        }
+        public void slowDown(){velX /= 2; velY /= 2; wind /= 2;}
+        public void speedUp(){velX *= 2;velY *= 2; wind *= 2;}
+
+        public float getX(){return x;}
+        public float getY(){return y;}
+        public float getW(){return w;}
+        public float getH(){return h;}
+        public double getWindSpeed(){return wind;}
+
+        public WindLine[] getWindLines(){return lines;}
+
+        class WindLine{
+            float wX1, wY, wX2;
+            WindLine(float X, float Y){
+                wX1 = X; wY = Y;
+                wX2 = X + (float)(wind * 3);
+            }
+
             public void update(){
-                x += s;
+                if (wind < 0){
+                    if (wX2 <= x - (w / 2) ){
+                        wY = random(y-h, y+h);
+                        wX1 = x + (w / 2);
+                    }
+                } else if (wind > 0){
+                    if (wX2 >= x + (w / 2) ){
+                        wY = random(y-h, y+h);
+                        wX1 = x - (w / 2);
+                    }
+                }
+                wX1 += wind;
             }
+
+            public float getX1(){return wX1;}
+            public float getX2(){return wX2;}
+            public float getY(){return wY;}
         }
     }
 
-    static class health{
-        float x, y, velX, velY;
+    static class Ice{
         int w, h;
+        Ice(float X, float Y, int width, int height, float YSpeed){
+            x = X; y = Y;
+            velY = YSpeed;
+            w = width; h = height;
+        }
+        public void update(){x += velX; y += velY;}
+        public void slowDown(){velX /= 2; velY /= 2;}
+        public void speedUp(){velX *= 2;velY *= 2;}
+    }
+
+    static class Health{
+        Health(float X, float Y, float XSpeed, float YSpeed){
+            x = X; y = Y;
+            velX = XSpeed; velY = YSpeed;
+        }
+        public void update(){x += velX; y += velY;}
+        public void slowDown(){velX /= 2; velY /= 2;}
+        public void speedUp(){velX *= 2;velY *= 2;}
+    }
+
+    public static void update(){
+        x += velX; y += velY;
     }
 }
--- Sketch.java
+++ Sketch.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
@@ -8,9 +9,20 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution, put your size all in this method
    */
   ArrayList<Objects.Snowball>snowballs = new ArrayList<Objects.Snowball>();
+  ArrayList<Objects.Wall>walls = new ArrayList<Objects.Wall>();
+  ArrayList<Objects.Wind>winds = new ArrayList<Objects.Wind>();
+  
+  boolean clicked = false;
+
+  PImage heart;
+
+  float pX = 500, pY = 600, pVelX, pVelY;
+  int pSpeed = 3;
+  int lives = 3;
+  double slow = 100;
+  double slowChange = 0.001;
 
   public void settings() {
-	// put your size call here
     size(1000, 800);
   }
 
@@ -20,38 +32,143 @@ public class Sketch extends PApplet {
    */
   
   public void setup() {
-    background(65);
-    snowballs.add(new Objects.Snowball (500,400,0,0,10,0.2) );
+    heart = loadImage("Heart.png");
+
+    //winds.add(new Objects.Wind(300, 500, 100, 60, 0, 0, -1) );
+    snowballs.add(new Objects.Snowball(500,400,0,0,10,0.2) );
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
+    background(0);
+
+    if (slow <= 0){}
+
     fill (255); stroke(128);
-    for (Objects.Snowball ball : snowballs){
-      ball.update();
-	    displaySnowball(ball);
+    for (Objects.Snowball snow : snowballs){
+      if (snow.getY() + snow.getR() > height){
+        snowballs.remove(snow);
+      }
+      snow.update();
+	    displaySnowball(snow);
     }
+
+    stroke(128);
+    for (Objects.Wind wind : winds){
+      wind.update();
+      displayWind(wind);
+    }
+
+    displaySlow();
+    displayLife(lives);
   }
   
   public void keyPressed() {
-      
+    if ('w' == key){pVelY = -pSpeed;}
+    if ('a' == key){pVelX = -pSpeed;}
+    if ('s' == key){pVelY = pSpeed;}
+    if ('d' == key){pVelX = pSpeed;}
   }
 
-  public void mouseClicked(){
+  public void keyReleased(){
+    if ('w' == key || 's' == key){pVelY = 0;}
+    if ('a' == key || 'd' == key){pVelX = 0;}
 
+    if (keyCode == DOWN){
+      for (Objects.Snowball snow : snowballs){snow.speedUp();}
+      for (Objects.Wall wall : walls){wall.speedUp();}
+      for (Objects.Wind wind : winds){wind.speedUp();}
+    }
+    if (keyCode == UP){
+      for (Objects.Snowball snow : snowballs){snow.slowDown();}
+      for (Objects.Wall wall : walls){wall.slowDown();}
+      for (Objects.Wind wind : winds){wind.slowDown();}
+    }
+  }
+
+  public void mousePressed(){
+    clicked = true;
+    for (Objects.Snowball ball : snowballs){
+      if (collision(mouseX, ball.getX(), mouseY, ball.getY(), ball.getR(), 0)){
+        ball.hide();
+        slow += 1;
+      }
+    }
+  }
+
+  public void mouseReleased(){
+    clicked = false;
   }
 
   public void initalize(){
 
   }
 
+  // object methods go below here
+  
+
+  // display methods go below here
   public void displaySnowball(Objects.Snowball snowball){
-    circle(snowball.getX(), snowball.getY(), snowball.getR() );
+    if (!snowball.hidden){circle(snowball.getX(), snowball.getY(), snowball.getR() );}
   }
 
   public void displayWall(Objects.Wall wall){
+    rect(wall.getX() - (wall.getW() / 2), wall.getY() - (wall.getH() / 2), wall.getX() + (wall.getW() / 2), wall.getY() + (wall.getH() / 2) );
+  }
 
+  public void displayWind(Objects.Wind wind){
+    Objects.Wind.WindLine[] winds = wind.getWindLines();
+    for (Objects.Wind.WindLine line : winds){
+      float displayX1, displayX2;
+
+      if (wind.getWindSpeed() < 0){
+        if (line.getX1() <= wind.getX() - (wind.getW() / 2) ){
+          displayX1 = (wind.getX() - (wind.getW() / 2) );
+        } else {displayX1 = line.getX1();}
+        if (line.getX2() >= wind.getX() + (wind.getW() / 2) ){
+          displayX2 = (wind.getX() + (wind.getW() / 2) );
+        } else {displayX2 = line.getX2();}
+        line(displayX1, line.getY(), displayX2, line.getY() );
+      }
+      else if (wind.getWindSpeed() > 0){
+        if (line.getX1() >= wind.getX() + (wind.getW() / 2) ){
+          displayX1 = (wind.getX() + (wind.getW() / 2) );
+        } else {displayX1 = line.getX1();}
+        if (line.getX2() <= wind.getX() - (wind.getW() / 2) ){
+          displayX2 = (wind.getX() - (wind.getW() / 2) );
+        } else {displayX2 = line.getX2();}
+        line(displayX1, line.getY(), displayX2, line.getY() );
+      }
+    }
+  }
+
+  public void displayPlayer(float playerX, float playerY){
+    circle(playerX, playerY, 30);
+  }
+
+  // UI elements go below here
+  public void displayLife(int lives){
+    for (int i = 0; i < lives; i++){
+      image(heart, width - 60 - (i * 55), 20);
+    }
+  }
+
+  public void displaySlow(){
+    fill(0); stroke(128);
+    rect(825, 80, 165, 20);
+    fill(0,0,200); stroke(255);
+    rect(827, 82, (float)(161 * (slow/100) ), 16);
+  }
+
+  // general math methods go below here
+  public boolean collision(float X1, float X2, float Y1, float Y2, float R1, float R2){
+    return ( Math.sqrt(Math.pow(Math.abs(Y1 - Y2), 2) + Math.pow(Math.abs(X1 - X2), 2) ) <= R1 + R2);
+  }
+
+  public boolean offScreen(float x, float y, float maxSize){
+    float size = maxSize * 2;
+    return ( (x + size < width || x - size > width)||(y + size < height || y - size > height) );
   }
 }
